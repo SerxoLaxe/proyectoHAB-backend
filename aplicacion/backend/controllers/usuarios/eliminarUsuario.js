87c55d7ab54ce0078189acb0f581742ce46b3904
Sergio_Lage <serxolaxe@gmail.com>
2021-08-20 10:43:17 +0200

    Añadido controlador eliminarUsuario.js

--- aplicacion/backend/controllers/usuarios/eliminarUsuario.js
+++ aplicacion/backend/controllers/usuarios/eliminarUsuario.js
@@ -1,18 +1,33 @@
+const conexionMysql = require('../../DB/conexionMysql');
+
 /**
- * Elimina un usuario. ❌
+ * Elimina un usuario 👍 
  * @param {*} req 
  * @param {*} res 
  * @param {*} next 
  */
 async function eliminarUsuario(req, res, next) {
+    let conexion;
     try {
+        conexion = await conexionMysql();
+        await conexion.query(
+            `
+            DELETE FROM usuarios WHERE id=?
+            `,
+            [req.params.id])
+
         res.statusCode = 200;
         res.send({
             status: 'Ok',
-            message: 'eliminar usuario',
+            message: `Eliminado usuario  con id ${req.params.id}`
         });
     } catch (error) {
-        next(error)
+        next(error);
+    } finally {
+        if (conexion) {
+            conexion.release();
+        }
     }
 }
+
 module.exports = eliminarUsuario;
