0d290d06ca1878c8ab91fa463d8b4ef3ed18c3c0
serxolaxe <serxolaxe@gmail.com>
2021-08-29 20:37:11 +0200

    editados comentarios para ser más descriptivos

--- aplicacion/backend/schemas/index.js
+++ aplicacion/backend/schemas/index.js
@@ -5,29 +5,29 @@ const añadirExperienciaSchema = Joi.object().keys({
 
     body: Joi.object().required().keys({
         nombre: Joi.string().required().error(
-            new Error('El nombre es obligatorio.')
+            new Error('El nombre es un campo obligatorio.')
         ),
         descripcion: Joi.string().required().max(joiConfig.schemaExperiencias.charsDescripcion).error(
             new Error(`La descripción es obligatoria y debe tener menos de ${joiConfig.schemaExperiencias.charsDescripcion} carácteres de extensión.`)
         ),
         fecha_inicial: Joi.string().required().isoDate().error(
-            new Error('La fecha de inicio es obligatoria y debe tener formato AAAA/MM/DD.')
+            new Error('La fecha de inicio es un campo obligatorio y debe tener formato AAAA/MM/DD.')
         ),
         fecha_final: Joi.string().required().isoDate().error(
-            new Error('La fecha final es oblogatoria y debe tener formato AAAA/MM/DD.')
+            new Error('La fecha final es un campo obligatorio oblogatoria y debe tener formato AAAA/MM/DD.')
         ),
         precio: Joi.number().required().positive().error(
-            new Error('El precio es obligatorio y debe ser de valor positivo.')
+            new Error('El precio es un campo obligatorio y debe ser de valor positivo.')
         ),
         ubicacion: Joi.string().required().error(
-            new Error('La ubicación es obligatoria.')
+            new Error('La ubicación un campo obligatorio.')
         ),
         plazas_totales: Joi.number().required().positive().error(
-            new Error('Las plazas totales son obligatorias.')
+            new Error('Las plazas totales son obligatorias y debe ser de valor positivo.')
         ),
     }),
     files: Joi.object().required().max(joiConfig.schemaExperiencias.maxImagenes).error(
-        new Error(`Sólo se permite un máximo de ${joiConfig.schemaExperiencias.maxImagenes} imágenes por experiencia.`)
+        new Error(`Es obligatorio incluir al menos una imagen y sólo se permite un máximo de ${joiConfig.schemaExperiencias.maxImagenes} imágenes por experiencia.`)
     ).pattern(
         Joi.string(),
         Joi.object().keys({
