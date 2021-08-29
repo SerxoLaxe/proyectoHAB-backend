b1edcbf4f978a1aa19d33f764db6938100ca27aa
serxolaxe <serxolaxe@gmail.com>
2021-08-29 20:49:07 +0200

    Adicion de variable minImagenes y mejora de comentarios

--- aplicacion/backend/config.js
+++ aplicacion/backend/config.js
@@ -1,25 +1,25 @@
 /**
- * Configuración de generación de datos originados por Faker
+ * Configuración de generación de datos originados por Faker. configuración utilizada en ./DB/initDB.js
  */
 const fakerConfig = {
     usuarios: {
-        cantidad: 10,
-        parrafosBiografia: 2,
+        cantidad: 10,               //Cantidad de usuarios a introducir en la tabla.
+        parrafosBiografia: 2,       //Párrafos de lorem ipsum a introducir en la biografía.
     },
     experiencias: {
-        cantidad: 10,
-        parrafosDescripcion: 3,
+        cantidad: 10,               //Cantidad de experiencias a introducir en la tabla.
+        parrafosDescripcion: 3,     //Párrafos de lorem ipsum a introducir en la descripción.
         precio: {
-            maximo: 5,
-            minimo: 2,
+            maximo: 5,              //Precio máximo.
+            minimo: 2,              //Precio mínimo.
         },
         rating: {
-            maximo: 5,
-            minimo: 0,
+            maximo: 5,              //Rating máximo
+            minimo: 0,              //Rating mínimo
         },
         plazas: {
-            maximas: 34,
-            minimas: 2,
+            maximas: 34,            //Número de plazas máximo.
+            minimas: 2,             //Número de plazas mínimo.
         },
     }
 }
@@ -28,8 +28,9 @@ const fakerConfig = {
  */
 const joiConfig = {
     schemaExperiencias: {
-        charsDescripcion: 1000,
-        maxImagenes: 4,
+        charsDescripcion: 1000, //Carácteres máximos de extensión de la descripción.
+        maxImagenes: 4,         //Número máximo de imágenes por experiencia
+        minImagenes: 1,         //Número mínimo de imágenes por experiencia
     }
     
 }
