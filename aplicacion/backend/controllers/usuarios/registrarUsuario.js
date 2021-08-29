027341acfe47bbb23ab55dd5e8ca3b3133578232
serxolaxe <serxolaxe@gmail.com>
2021-08-29 23:24:58 +0200

    aplicación del esquema de validación en el controlador

--- aplicacion/backend/controllers/usuarios/registrarUsuario.js
+++ aplicacion/backend/controllers/usuarios/registrarUsuario.js
@@ -1,5 +1,6 @@
 const conexionMysql = require('../../DB/conexionMysql');
-
+const { validate } = require('../../helpers');
+const {registrarUsuarioSchema} = require('../../schemas/index')
 /**
  * Registra un nuevo usuario tomando del body de la petición el email y la contraseña, genera para el usuario un código de registro. ❌
  * @param {*} req 
@@ -7,8 +8,9 @@ const conexionMysql = require('../../DB/conexionMysql');
  * @param {*} next 
  */
 async function registrarUsuario(req, res, next) {
-    let conexion
+    let conexion;
     try {
+        await validate(registrarUsuarioSchema,req.body);
         conexion = await conexionMysql();
         res.statusCode = 200;
         res.send({
