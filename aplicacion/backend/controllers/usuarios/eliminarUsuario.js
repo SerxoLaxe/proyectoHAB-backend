172a518fb42d390a36658219a8750ce801a652f6
serxolaxe <serxolaxe@gmail.com>
2021-09-05 16:18:28 +0200

    Implantado borrado lógico de usuarios

--- aplicacion/backend/controllers/usuarios/eliminarUsuario.js
+++ aplicacion/backend/controllers/usuarios/eliminarUsuario.js
@@ -10,11 +10,38 @@ async function eliminarUsuario(req, res, next) {
     let conexion;
     try {
         conexion = await conexionMysql();
+
+        // sacar el id usuario que queremos eliminar.
+        const { id } = req.params;
+
+        console.log("ID usuario", id);
+
+        // se el usuario que quiero borrar el el id=1 (admin) salgo con error
+        if (Number(id) === 1) {
+            const error = new Error("El administrador no se pude eliminar");
+            error.httpStatus = 403;
+            throw error;
+        }
+
+        // si no es admin o el usuario que quiero borrar no coincide con el usuario de login salgo con un error
+        if (req.userAuth.role !== "admin" && req.userAuth.id !== Number(id)) {
+            const error = new Error(
+                "No tienes los permisos para eliminar este usuario"
+            );
+            error.httpStatus = 403;
+            throw error;
+        }
+
+        // hacemos el borrado logico del usuario:
+        // UPDATE: deleted = 1, password="[BORRADO]", name="[BORRADO]", avatar=NULL, active=0, lastAuthUpdate=new Date()
         await conexion.query(
             `
-            DELETE FROM usuarios WHERE id=?
-            `,
-            [req.params.id])
+        UPDATE usuarios
+        SET contraseña="[BORRADO]", nombre="[BORRADO]", biografia="[BORRADO] ,avatar=NULL, activo=0, eliminado=1, ultimo_cambio_contraseña=?
+        WHERE id=?
+    `,
+            [new Date(), id]
+        );
 
         res.statusCode = 200;
         res.send({
