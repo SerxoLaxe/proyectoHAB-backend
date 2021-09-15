a1c438791fdb58d6b9c23011f5f650993c441116
cristina <dominguez.ac@gmail.com>
2021-09-15 17:25:06 +0200

    Elimina experiencia solo administrador

--- controllers/usuarios/eliminarUsuario.js
+++ controllers/usuarios/eliminarUsuario.js
@@ -1,58 +1,59 @@
-const conexionMysql = require('../../DB/conexionMysql');
+const conexionMysql = require("../../DB/conexionMysql");
 
 /**
- * Elimina un usuario 👍 
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * Elimina un usuario 👍
+ *
+ * @param {any} req
+ * @param {any} res
+ * @param {any} next
  */
 async function eliminarUsuario(req, res, next) {
-    let conexion;
-    try {
-        conexion = await conexionMysql();
+  let conexion;
+  try {
+    conexion = await conexionMysql();
 
-        // Sacar el id usuario que queremos eliminar.
-        const { id } = req.params;
+    // Sacar el id usuario que queremos eliminar.
+    const { id } = req.params;
 
-        // Si el usuario que quiero borrar el el id=1 (admin) salgo con error.
-        if (Number(id) === 1) {
-            const error = new Error("El administrador no se pude eliminar");
-            error.httpStatus = 403;
-            throw error;
-        }
+    // Si el usuario que quiero borrar el el id=1 (admin) salgo con error.
+    if (Number(id) === 1) {
+      const error = new Error("El administrador no se pude eliminar");
+      error.httpStatus = 403;
+      throw error;
+    }
 
-        // Si no es admin o el usuario que quiero borrar no coincide con el usuario de login salgo con un error.
-        if (req.userAuth.role !== "admin" && req.userAuth.id !== Number(id)) {
-            const error = new Error(
-                "No tienes los permisos para eliminar este usuario"
-            );
-            error.httpStatus = 403;
-            throw error;
-        }
+    // Si no es admin o el usuario que quiero borrar no coincide con el usuario de login salgo con un error.
+    if (req.userAuth.role !== "admin" && req.userAuth.id !== Number(id)) {
+      const error = new Error(
+        "No tienes los permisos para eliminar este usuario"
+      );
+      error.httpStatus = 403;
+      throw error;
+    }
 
-        // Hacemos el borrado logico del usuario:
-        // UPDATE: deleted = 1, password="[BORRADO]", name="[BORRADO]", avatar=NULL, active=0, lastAuthUpdate=new Date().
-        await conexion.query(
-            `
+    // Hacemos el borrado logico del usuario:
+    // UPDATE: deleted = 1, password="[BORRADO]", name="[BORRADO]", avatar=NULL, active=0, lastAuthUpdate=new Date().
+    await conexion.query(
+      `
         UPDATE usuarios
         SET contraseña="[BORRADO]", nombre="[BORRADO]", email="[BORRADO]" ,biografia="[BORRADO]" ,avatar=NULL, activo=0, eliminado=1, ultimo_cambio_contraseña=?
         WHERE id=?
     `,
-            [new Date(), id]
-        );
+      [new Date(), id]
+    );
 
-        res.statusCode = 200;
-        res.send({
-            status: 'Ok',
-            message: `Eliminado usuario  con id ${req.params.id}`
-        });
-    } catch (error) {
-        next(error);
-    } finally {
-        if (conexion) {
-            conexion.release();
-        }
+    res.statusCode = 200;
+    res.send({
+      status: "Ok",
+      message: `Eliminado usuario  con id ${req.params.id}`,
+    });
+  } catch (error) {
+    next(error);
+  } finally {
+    if (conexion) {
+      conexion.release();
     }
+  }
 }
 
 module.exports = eliminarUsuario;
